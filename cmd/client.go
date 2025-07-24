cb3db03c24e31a3dc5802ab1b69df6f5bd48b007
Alvise Favero <66018479+alvi-se@users.noreply.github.com>
2025-07-24 15:01:14 +0200

    feat: working example of QUIC server and client

--- cmd/client.go
+++ cmd/client.go
@@ -10,12 +10,15 @@ import (
 )
 
 func main() {
+	// Remote server to connect to
 	addr, err := net.ResolveUDPAddr("udp4", "localhost:6969")
 
 	if err != nil {
 		log.Fatalf("Error resolving UDP address: %v", err)
 	}
 
+	// Local UDP socket
+	// The second argument is nil to make the app listen to any IP and random port
 	udpConn, err := net.ListenUDP("udp4", nil)
 
 	if err != nil {
@@ -24,7 +27,19 @@ func main() {
 
 	tr := quic.Transport{Conn: udpConn}
 
-	conn, err := tr.Dial(context.Background(), addr, &tls.Config{}, &quic.Config{})
+	cert, err := tls.LoadX509KeyPair("tls/client.crt", "tls/client.key")
+
+	if err != nil {
+		log.Fatalf("Error loading TLS certificate: %v", err)
+	}
+
+	conn, err := tr.Dial(context.Background(), addr,
+		&tls.Config{
+			Certificates: []tls.Certificate{cert},
+			// This is just for testing, the client needs to verify the server's certificate
+			InsecureSkipVerify: true,
+		},
+		&quic.Config{})
 
 	if err != nil {
 		log.Fatalf("Error dialing QUIC connection: %v", err)
@@ -32,6 +47,7 @@ func main() {
 
 	log.Println("QUIC client connected to", conn.RemoteAddr())
 
+	// Bidirectional stream
 	str, err := conn.OpenStream()
 	defer str.Close()
 
@@ -39,10 +55,12 @@ func main() {
 		log.Fatalln("Error opening stream:", err)
 	}
 
+	// conn.OpenStream doesn't send anything to the server
+	// so the server doesn't know when the stream is open until
+	// the client sends data
 	str.Write([]byte("Hello from QUIC client!"))
 
-	var buf []byte
+	buf := make([]byte, 1024)
 	n, err := str.Read(buf)
 	log.Printf("Received %d bytes: %s\n", n, buf[:n])
 }
-
